02b3d54566ebab8b7a1aaf9a5ac985e7a6297912
pfistfl <pfistererf@googlemail.com>
2018-10-23 15:59:11 +0200

    setup

--- R/setup_runs.R
+++ R/setup_runs.R
@@ -10,8 +10,16 @@ reg = load_or_create_registry("randombot_reg", overwrite = FALSE)
 runBot = function(task.id) {
   bot = RandomBot$new(as.character(task.id))
   bot$run()
+  # FIXME: How do we save to disk?
 }
 
-for (task in task.ids) {
-  addProblem(name = task, fun = runBot, seed = 42)
+for (task.id in task.ids) {
+  addProblem(name = as.character(task.id), data = list(task.id = as.character(task.id)), seed = 42)
 }
+
+addAlgorithm("randomBot", fun = function(job, data, instance, ...) {
+  result = runBot(data$task.id)
+  return(result)
+})
+
+addExperiments(repls = REPLS)
