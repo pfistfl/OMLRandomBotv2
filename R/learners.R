7100c061fb2aa1ce290ac3b33c7b9484a0b6e23b
pfistfl <pfistererf@googlemail.com>
2018-10-23 16:35:53 +0200

    Create empty but required functions

--- R/learners.R
+++ R/learners.R
@@ -1,1 +1,11 @@
 # Definition of all learners and corresponding param sets
+
+#' List available learners for a given task
+list_learners = function(task) {
+
+}
+
+#' Create the paramset for a given learner or task
+make_parset = function(learner, task) {
+
+}
