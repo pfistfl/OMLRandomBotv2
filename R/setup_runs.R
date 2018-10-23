ddc7263692f0efd87b9ef3c7f7de166caa5ab163
pfistfl <pfistererf@googlemail.com>
2018-10-23 17:03:32 +0200

    Update setup

--- R/setup_runs.R
+++ R/setup_runs.R
@@ -1,16 +1,15 @@
 library(batchtools)
+
 source.pkgs = list("mlr", "R6", "OpenML", "data.table")
 source.files = c("Bot.R", "tasks.R", "learners.R", "helpers.R", "Bot.R")
 sapply(source, source.files)
+
 reg = load_or_create_registry("randombot_reg", overwrite = FALSE,
   source.files = source.files, source.pkgs = source.pkgs)
 
 #' Function that runs the Bot
 runBot = function(task.id) {
-  bot = RandomBot$new(as.character(task.id))
-  bot$run()
-  # FIXME: How do we save to disk?
-  # 	   I guess via batchtools result
+
 }
 
 for (task.id in task.ids) {
@@ -18,7 +17,10 @@ for (task.id in task.ids) {
 }
 
 addAlgorithm("randomBot", fun = function(job, data, instance, ...) {
-  result = runBot(data$task.id)
+  bot = RandomBot$new(as.character(data$task.id))
+  # FIXME: How do we save to disk?
+  # 	   I guess via batchtools result.
+  result = bot$run()
   return(result)
 })
 
