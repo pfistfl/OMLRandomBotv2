d3304b5ea0cbd24ccc1fda5571fb1399e39a0d36
pfistfl <pfistererf@googlemail.com>
2018-10-24 10:08:48 +0200

    Setup

--- R/setup_runs.R
+++ R/setup_runs.R
@@ -4,17 +4,20 @@ source("learners.R")
 source("helpers.R")
 source("Bot.R")
 
+# FIXME: Learner Packages
+# FIXME: Source files
 reg = load_or_create_registry("randombot_reg", overwrite = FALSE)
 
 #' Function that runs the Bot
 runBot = function(task.id) {
   bot = RandomBot$new(as.character(task.id))
   bot$run()
-  # FIXME: How do we save to disk?
+  # FIXME: How do we save to disk? Just the return value?
+  # FIXME: Check size of the OML Run
 }
 
 for (task.id in task.ids) {
-  addProblem(name = as.character(task.id), data = list(task.id = as.character(task.id)), seed = 42)
+  addProblem(name = task.id, data = list(task.id = task.id), seed = 42)
 }
 
 addAlgorithm("randomBot", fun = function(job, data, instance, ...) {
